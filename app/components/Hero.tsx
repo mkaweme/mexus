01bc5fc3cd1d700b077d33b0f9a6e5633c0c7b39
Makoba Kaweme <csmkay@gmail.com>
2024-10-05 02:48:20 +0200

    First commit

---
+++ app/components/Hero.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div className='h-screen bg-[url("/assets/metal_fab_resized.jpg")]'>
+      <div className="flex flex-col text-white align-middle mx-16">
+        <h4 className="text-[30px] mt-64">We are</h4>
+        <h1 className="text-[54px] font-semibold ">Mexus Industrial Solutions</h1>
+        <h4 className="text-[24px] mt-4 w-[450px]">Your trusted partner for Mechanical and Electrical engineering solutions
+        </h4>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
