e27e86da937c059a20b9ac7de96628873dfb59ac
Makoba Kaweme <csmkay@gmail.com>
2024-10-08 04:52:17 +0200

    Added images

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
 import React from "react";
+import Maintenance from "@/public/assets/maintenance.jpg";
+import Bolts from "@/public/assets/bolts.jpg";
 
 const Services = () => {
   return (
@@ -12,25 +15,35 @@ const Services = () => {
           </p>
         </div>
         <div>
-          <p className="text-[36px] my-3"> Our services include: </p>
-          <ul className="flex flex-col text-[20px] list-outside list-disc gap-4">
-            <li>Steel Fabrication: Structural steel, tanks, sheet metalwork etc.</li>
-            <li>Plant intallation and maintenance.</li>
-            <li>Plant shutdown maintenance</li>
-            <li>Mechanical survey, inspection and consultancy</li>
-            <li>Industrial equipment and spare parts supply</li>
-            <li>Pulley rubber lagging and belt splicing</li>
-            <li>Gearbox, cylinders and pump repair and overhaul</li>
-          </ul>
-          <p className="text-[36px] my-3">We supply mechanical consumables: </p>
-          <ul className="flex flex-col text-[20px] list-outside list-disc gap-4">
-            <li>Lubricants</li>
-            <li>Belts.</li>
-            <li>Fasteners</li>
-            <li>Couplings</li>
-            <li>Bearings</li>
-            <li>Welding rods</li>
-          </ul>
+          <div className="flex flex-col lg:flex-row my-2">
+            <div className="flex flex-col">
+              <p className="text-[36px] my-3"> Our services include: </p>
+              <ul className="flex flex-col text-[20px] list-outside list-disc gap-4">
+                <li>Steel Fabrication: Structural steel, tanks, sheet metalwork etc.</li>
+                <li>Plant intallation and maintenance.</li>
+                <li>Plant shutdown maintenance</li>
+                <li>Mechanical survey, inspection and consultancy</li>
+                <li>Industrial equipment and spare parts supply</li>
+                <li>Pulley rubber lagging and belt splicing</li>
+                <li>Gearbox, cylinders and pump repair and overhaul</li>
+              </ul>
+            </div>
+            <Image src={Maintenance} alt="maintenance" className="rounded-full w-[450px] h-auto mx-32 border-8 border-[#e46c0a]"/>
+          </div>
+          <div className="flex flex-col lg:flex-row my-4">
+            <div className="flex flex-col">
+              <p className="text-[36px] my-3">We supply mechanical consumables: </p>
+              <ul className="flex flex-col text-[20px] list-outside list-disc gap-4">
+                <li>Lubricants</li>
+                <li>Belts.</li>
+                <li>Fasteners</li>
+                <li>Couplings</li>
+                <li>Bearings</li>
+                <li>Welding rods</li>
+              </ul>
+            </div>
+            <Image src={Bolts} alt="bolts" className="rounded-full w-[400px] h-auto mx-32 border-8 border-[#e46c0a]"/>
+          </div> 
         </div>
       </div>
     </>
