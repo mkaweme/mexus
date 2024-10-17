c07527e9e5ee62aa72c2854d3622be73fbd6fa1a
Makoba Kaweme <csmkay@gmail.com>
2024-10-17 20:46:05 +0200

    Improved styling to figma design

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,8 +1,8 @@
 const Hero = () => {
   return (
-    <div className='h-screen bg-[url("/assets/metal_fab_resized.jpg")]'>
+    <div className='h-screen bg-[url("/assets/metal_fab_resized.jpg")] animate-fade-in'>
       <div className="flex flex-col text-white align-middle mx-16">
-        <h4 className="text-[30px] mt-64">We are</h4>
+        <h4 className="text-[30px] mt-56">We are</h4>
         <h1 className="text-[54px] font-semibold ">Mexus Industrial Solutions</h1>
         <h4 className="text-[24px] mt-4 w-[450px]">Your trusted partner for Mechanical and Electrical engineering solutions
         </h4>
