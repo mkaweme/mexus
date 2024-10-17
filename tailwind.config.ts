0a6c165232b6ebcde567da3ee2070e8a149d8601
Makoba Kaweme <csmkay@gmail.com>
2024-10-17 20:44:39 +0200

    added: fading in project images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,28 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        "fade-in": {
+          "0%": {
+              opacity: "0%",
+            },
+            "100%": {
+                opacity: "100%",
+            }
+        },
+        "fade-out": {
+          "100%": {
+              opacity: "100%",
+            },
+            "0%": {
+                opacity: "0%",
+            }
+        }
+      },
+      animation: {
+        "fade-in": "fade-in 1.5s ease-in-out",
+        "fade-out": "fade-out 1.5s ease-in-out",
+      }
     },
   },
   plugins: [],
