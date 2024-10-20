a8df38044f3bcbcdce23cd0fd662e54b42cc32a1
Makoba Kaweme <csmkay@gmail.com>
2024-10-20 08:13:17 +0200

    Removed services

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@ import ChooseUs from "./components/ChooseUs";
 import Clients from "./components/Clients";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
-import Services from "./components/Services";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      {/* <Services /> */}
       <ChooseUs />
       <Projects />
       <Clients />
