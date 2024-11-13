57d83e2030d212ab1f77b86bca37e2843aa2649b
Kaweme <67505756+mkaweme@users.noreply.github.com>
2024-11-13 10:19:19 +0200

    improved UI/UX according to new figma design

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,26 +1,32 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import NFCA2 from "@/public/assets/nfca_2.jpg";
 import BAT1 from "@/public/assets/bat_1.jpg";
 import BAT3 from "@/public/assets/bat_3.jpg";
 
+type project = {
+  src: StaticImageData;
+  title: string;
+}
+
+const PROJECTS: project[] = [
+  { src: NFCA2, title: "Installation of impact bed"  },
+  { src: BAT1, title: "Installation of an Eye wash station" },
+  { src: BAT3, title: "Repair of a Tobacco Feeder"  },
+];
 const Projects = () => {
 
+  //Create state variables
   const [currentImage, setCurrentImage] = useState(0);
   const [fade, setFade] = useState("fade-in");
 
-  const images = [
-    { src: NFCA2, title: "Installation of impact bed"  },
-    { src: BAT1, title: "Installation of an Eye wash station" },
-    { src: BAT3, title: "Repair of a Tobacco Feeder"  },
-  ];
-
+  //Create a useEffect for running the function that changes the image
   useEffect(() => {
     const interval = setInterval(() => {
       setFade("fade-out");
       setTimeout(() => {
-        setCurrentImage((prev) => (prev + 1) % images.length);
+        setCurrentImage((prev) => (prev + 1) % PROJECTS.length);
         setFade("fade-in");
       }, 1000);
     }, 4000);
@@ -29,11 +35,10 @@ const Projects = () => {
 
   return (
     <>
-      <div className="relative flex flex-col w-full h-screen my-4 items-center align-middle">
-        <h1 className="text-[48px] font-bold text-[#e46c0a]">Projects</h1>
-        <p className="text-[20px] my-4">Some of our past projects.</p>
-        <div className="relative flex flex-col-reverse w-[600px] mx-20 my-10 z-0 bg-gray-500">
-          {images.map((image, index) => (
+      <div className="relative flex flex-col w-full h-screen items-center align-middle">
+        <h1 className="text-[32px] lg:text-[40px] font-bold text-[#F19221] my-10"><span className="text-black border-b-4 border-black">OUR</span> PROJECTS</h1>
+        <div className="relative flex flex-col-reverse w-[250px] lg:w-[550px] h-auto mx-20 my-8 z-0">
+          {PROJECTS.map((image, index) => (
             <div
               key={index}
               className={`absolute bottom-0 left-0 w-full h-full transition-opacity duration-1000 ease-in-out z-10 ${
@@ -47,26 +52,26 @@ const Projects = () => {
                 height={500}
                 className="w-full h-auto object-cover"
               />
-              <div className="absolute bottom-0 left-0 w-full font-bold my-3 p-2 text-center">
+              <div className="absolute w-full font-bold my-6 lg:my-3 p-2 text-center">
                 <h2 className="text-lg">{image.title}</h2>
               </div>
             </div>
           ))}
-          <div className="absolute top-[-20px] left-[-20px]">
-            <div className="bg-[#F39224] w-[120px] h-[120px]"></div>
-            <div className="absolute bottom-0 right-0 bg-white w-[110px] h-[110px]"></div>
+          <div className="absolute -top-[20px] -left-[20px]">
+            <div className="bg-[#F39224] w-[60px] lg:w-[120px] h-[60px] lg:h-[120px]"></div>
+            <div className="absolute bottom-0 right-0 bg-white w-[50px] lg:w-[110px] h-[50px] lg:h-[110px]"></div>
           </div>
-          <div className="absolute top-[-20px] right-[-20px]">
-            <div className="bg-[#F39224] w-[120px] h-[120px]"></div>
-            <div className="absolute bottom-0 left-0 bg-white w-[110px] h-[110px]"></div>
+          <div className="absolute -top-[20px] -right-[20px]">
+            <div className="bg-[#F39224] w-[60px] lg:w-[120px] h-[60px] lg:h-[120px]"></div>
+            <div className="absolute bottom-0 left-0 bg-white w-[50px] lg:w-[110px] h-[50px] lg:h-[110px]"></div>
           </div>
-          <div className="absolute bottom-[-470px] left-[-20px]">
-            <div className="bg-[#F39224] w-[120px] h-[120px]"></div>
-            <div className="absolute top-0 right-0 bg-white w-[110px] h-[110px]"></div>
+          <div className="absolute -bottom-[210px] lg:-bottom-[435px] -left-[20px]">
+            <div className="bg-[#F39224] w-[60px] lg:w-[120px] h-[60px] lg:h-[120px]"></div>
+            <div className="absolute top-0 right-0 bg-white w-[50px] lg:w-[110px] h-[50px] lg:h-[110px]"></div>
           </div>
-          <div className="absolute bottom-[-470px] right-[-20px]">
-            <div className="bg-[#F39224] w-[120px] h-[120px]"></div>
-            <div className="absolute top-0 left-0 bg-white w-[110px] h-[110px]"></div>
+          <div className="absolute -bottom-[210px] lg:-bottom-[435px] -right-[20px]">
+            <div className="bg-[#F39224] w-[60px] lg:w-[120px] h-[60px] lg:h-[120px]"></div>
+            <div className="absolute top-0 left-0 bg-white w-[50px] lg:w-[110px] h-[50px] lg:h-[110px]"></div>
           </div>
         </div>
       </div>
