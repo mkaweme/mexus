4ffe8be19ea0262069a00dd29e48bbe79d9375a8
Kaweme <67505756+mkaweme@users.noreply.github.com>
2024-11-13 10:18:06 +0200

    improved UI/UX according to new figma design

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,11 +1,15 @@
+import React from "react";
+
 const Hero = () => {
   return (
     <div className='h-screen bg-[url("/assets/metal_fab_resized.jpg")] animate-fade-in'>
-      <div className="flex flex-col text-white align-middle mx-16">
-        <h4 className="text-[30px] mt-56">We are</h4>
-        <h1 className="text-[54px] font-semibold ">Mexus Industrial Solutions</h1>
-        <h4 className="text-[24px] mt-4 w-[450px]">Your trusted partner for Mechanical and Electrical engineering solutions
-        </h4>
+      <div className="flex justify-center items-center w-full h-full bg-[#14194978]">
+        <div className="flex flex-col text-white align-middle items-center lg:items-start lg:mb-20">
+          <h4 className="text-[30px] font-medium">We are</h4>
+          <h1 className="text-[32px] lg:text-[54px] text-center lg:text-start my-6 font-bold lg:w-[50%] text-[#F39224]">MEXUS INDUSTRIAL SOLUTIONS LIMITED</h1>
+          <h4 className="text-[14px] lg:text-[16px] text-center lg:text-start mt-4 lg:w-[50%]">Your trusted partner for Mechanical and Electrical engineering solutions
+          </h4>
+        </div>
       </div>
     </div>
   );
