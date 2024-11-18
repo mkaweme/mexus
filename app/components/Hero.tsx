c4595d10da2a5b77009050e7aa2a609a37a10ee4
Kaweme <67505756+mkaweme@users.noreply.github.com>
2024-11-18 23:37:00 +0200

    added poppins font for headings and subheadings

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,10 +7,10 @@ const Hero: React.FC = () => {
       <div className="flex justify-center items-center w-full h-full bg-[#14194978]">
         <div className="flex flex-col text-white align-middle items-center lg:items-start lg:mb-20">
           <h4 className="text-[30px] font-medium">We are</h4>
-          <h1 className="text-[32px] lg:text-[54px] text-center lg:text-start my-6 font-bold lg:w-[50%] text-[#F39224]">
+          <h1 className="text-[32px] lg:text-[54px] font-poppins text-center lg:text-start my-6 font-bold lg:w-[50%] text-[#F39224]">
             MEXUS INDUSTRIAL SOLUTIONS LIMITED
           </h1>
-          <h4 className="text-[14px] lg:text-[16px] text-center lg:text-start mx-4 lg:mx-0 mt-4 lg:w-[50%]">
+          <h4 className="text-[14px] lg:text-[16px] font-futurastdbook text-center lg:text-start mx-4 lg:mx-0 mt-4 lg:w-[50%]">
             Your trusted partner for Mechanical and Electrical engineering solutions
           </h4>
         </div>
