64b30a6bd315e83e4dfe5fa49dffaa83c3a2830a
Kaweme <67505756+mkaweme@users.noreply.github.com>
2024-11-18 23:35:18 +0200

    added futura and poppins font variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,43 @@
 import React from "react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ 
+  subsets: ["latin"], 
+  weight: ["400","500","700"],
+  variable: "--font-poppins",
+});
+
+const FuturaStdBook = localFont({
+  src: [
+    {
+      path: "../public/assets/fonts/FuturaStdBook.otf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-futurastdbook",
+});
+
+const FuturaStd = localFont({
+  src: [
+    {
+      path: "../public/assets/fonts/FuturaStdMedium.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/assets/fonts/FuturaStdBold.otf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-futurastd",
+});
 
 export const metadata: Metadata = {
   title: "Mexus Industrial Solutions",
@@ -19,7 +51,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${FuturaStd.className} ${FuturaStdBook.className} ${poppins.variable} $antialiased`}>
         <header className="sticky top-0 z-50">
           <Navbar />
         </header>
