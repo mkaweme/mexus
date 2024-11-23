ee8fa965743e3449b65cfad88086dc09215ca81e
Kaweme <67505756+mkaweme@users.noreply.github.com>
2024-11-23 18:39:19 +0200

    removed unused import, improved formatting

--- app/services/components/Projects.tsx
+++ app/services/components/Projects.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import Image, { StaticImageData } from "next/image";
+import { StaticImageData } from "next/image";
 import { Icon } from "@iconify/react";
 import NFCA1 from "@/public/assets/nfca_1.jpg";
 import NFCA2 from "@/public/assets/nfca_2.jpg";
@@ -30,7 +30,7 @@ const PROJECTS: Project[] = [
       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
       anim id est laborum.`,
     type: "mechanical",
-    images: [ NFCA1, NFCA2, NFCA3, NFCA4 ],
+    images: [NFCA1, NFCA2, NFCA3, NFCA4],
   },
   {
     title: "Installation of An Eye Wash Station",
@@ -48,7 +48,7 @@ const PROJECTS: Project[] = [
       curae; Pellentesque ornare urna eros, sed pretium lorem 
       sollicitudin quis`,
     type: "mechanical",
-    images: [ BAT1 , BAT2 , BAT3 , BAT4 ],
+    images: [BAT1, BAT2, BAT3, BAT4],
   },
   {
     title: "Repair of a Tobacco Feeder",
@@ -62,58 +62,53 @@ const PROJECTS: Project[] = [
       facilisis diam eu gravida. Donec convallis est a aliquet 
       maximus.`,
     type: "electrical",
-    images: [ BAT5 , BAT6 , BAT5, BAT6],
+    images: [BAT5, BAT6, BAT5, BAT6],
   },
 ];
 
 const Projects: React.FC = () => {
 
-  //Create current intdex state variable
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-
-  const gotoPrevImage = (project: Project) => {
-    const isFirstSlide: boolean = currentIndex === 0;
-    const newIndex: number = isFirstSlide ? project.images.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-  
-  const gotoNextImage = (project: Project) => {
-    const isLastSlide: boolean = currentIndex === project.images.length - 1;
-    const newIndex: number = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
-
   return (
-    <div className="flex flex-col w-full h-screen items-center align-middle">
-      <h1 className="text-[32px] lg:text-[40px] font-bold text-[#F19221] my-10"><span className="text-black border-b-4 border-[#F19221]">OUR</span> PROJECTS</h1>
-      <div className="mx-24">
+    <div className="flex flex-col w-full items-center align-middle">
+      <h1 className="text-[32px] lg:text-[40px] font-bold text-[#F19221] my-6 lg:my-10"><span className="text-black border-b-4 border-[#F19221]">OUR</span> PROJECTS</h1>
+      <div className="flex flex-col lg:[&>*:nth-child(even)]:flex-row-reverse mx-24">
         {
-          PROJECTS.map((project, index) => (
-            <div key={ index } className="flex flex-col lg:flex-row my-10 gap-x-12">
-              <div className="flex flex-col flex-1">
-                <h2 className="text-poppins text-[20px] font-semibold text-center">{ project.title }</h2>
-                <p className="my-10 font-light">{ project.description }</p>
+          PROJECTS.map((project, index) => {
+            const [currentIndex, setCurrentIndex] = useState<number>(0);
+            return (
+              <div key={ index } className="flex flex-col lg:flex-row my-4 lg:my-10 gap-x-12">
+                <div className="flex flex-col flex-1 lg:py-20">
+                  <h2 className="text-poppins text-[20px] font-semibold text-center">{ project.title }</h2>
+                  <p className="my-4 lg:my-10 font-light">{ project.description }</p>
+                </div>
+                <div className="flex flex-col items-center">
+                  <div style={ { backgroundImage: `url(${project.images[currentIndex].src})` } } className="flex flex-col w-[350px] lg:w-[550px] h-[350px] lg:h-[400px] items-center duration-500 rounded-md bg-cover bg-center">
+                  </div>
+                  <div className="flex flex-row my-2">
+                    { project.images.map((image: StaticImageData, index: number) => (
+                      <Icon key={ index } icon="icon-park-outline:dot" width="16" height="16" color={ index === currentIndex ? "#F19221" : "#D9D9D9" } />
+                    )) }
+                  </div>
+                  <div className="flex flex-row gap-x-6 my-3">
+                    <button className="bg-[#F19221] rounded-full" aria-label="previous image" type="button" onClick={ () => {
+                      const isFirstSlide: boolean = currentIndex === 0;
+                      const newIndex: number = isFirstSlide ? project.images.length - 1 : currentIndex - 1;
+                      setCurrentIndex(newIndex);
+                    } }>
+                      <Icon icon="iconamoon:arrow-left-2-light" width="24" height="24" />
+                    </button>
+                    <button className="bg-[#F19221] rounded-full" aria-label="next image" type="button" onClick={ () => {
+                      const isLastSlide: boolean = currentIndex === project.images.length - 1;
+                      const newIndex: number = isLastSlide ? 0 : currentIndex + 1;
+                      setCurrentIndex(newIndex);
+                    } }>
+                      <Icon icon="iconamoon:arrow-right-2-light" width="24" height="24" />
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div className="flex flex-col items-center">
-                <div style={ { backgroundImage: `url(${project.images[currentIndex].src})` } } className="flex flex-col w-[550px] h-[400px] items-center duration-500 rounded-md bg-cover bg-center">
-                  {/* <Image src={ project.images[0].pic } alt={ project.title } width={ 500 } height={ 500 } className="w-full h-auto object-cover" /> */ }
-                </div>
-                <div className="flex flex-row my-2">
-                  {project.images.map((image, index) => (
-                    <Icon key={ index } icon="icon-park-outline:dot" width="16" height="16" color={ index === currentIndex ? "#F19221" : "#D9D9D9" }/>
-                  ))}
-                </div>
-                <div className="flex flex-row gap-x-6 my-3">
-                  <button className="bg-[#F19221] rounded-full" aria-label="previous image" type="button" onClick={ () => gotoPrevImage(project) } >
-                    <Icon icon="iconamoon:arrow-left-2-light" width="24" height="24" />
-                  </button>
-                  <button className="bg-[#F19221] rounded-full" aria-label="next image" type="button" onClick={ () => gotoNextImage(project) }>
-                    <Icon icon="iconamoon:arrow-right-2-light" width="24" height="24" />
-                  </button>
-                </div>
-              </div>
-            </div>
-          ))
+            )
+          })
         }
       </div>
     </div>
