3f000db134fead2841b0927d15c469d2d7590025
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-01-22 21:19:38 +0200

    Static site config settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+};
 
 export default nextConfig;
